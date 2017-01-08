9b495aa21916fbf0c90cbc39ec766173e0bfc59e
sam <sam@silver42.karoo.co.uk>
2017-01-08 20:37:55 +0000

    Add version string for console, way to print out all funcs

--- Geometrize/dialog/scriptconsole.cpp
+++ Geometrize/dialog/scriptconsole.cpp
@@ -1,6 +1,7 @@
 #include "scriptconsole.h"
 #include "ui_scriptconsole.h"
 
+#include "chaiscript/chaiscript.hpp"
 #include "script/chaiscriptcreator.h"
 #include "script/scriptrunner.h"
 
@@ -17,11 +18,22 @@ public:
     {
         ui->setupUi(q);
 
+        ui->outputView->append("ChaiScript " + QString::fromStdString(m_engine->build_id()));
+        ui->outputView->append(tr("Type 'help' for a list of commands"));
+
         connect(ui->commandLine, &QLineEdit::returnPressed, [this]() {
             if(m_engine != nullptr) {
-                ui->outputView->append(ui->commandLine->text());
-                script::runScript(ui->commandLine->text().toStdString(), *m_engine, nullptr);
+                if(ui->commandLine->text() == "help") { // TODO?
+                    const auto state{m_engine->get_state()};
+                    const auto funcs{state.engine_state.m_functions};
+                    for(auto it = funcs.begin(); it != funcs.end(); ++it) {
+                        ui->outputView->append(QString::fromStdString(it->first));
+                    }
+                } else {
+                    script::runScript(ui->commandLine->text().toStdString(), *m_engine, nullptr);
+                }
             }
+            ui->outputView->append(ui->commandLine->text());
             ui->commandLine->clear();
         });
     }
