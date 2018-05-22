d726711da035eb8968650bf3893c17b3ab69130b
sam <sam@silver42.karoo.co.uk>
2018-05-22 21:35:12 +0100

    Minor tweaks

--- geometrize/cli/commandlineparser.cpp
+++ geometrize/cli/commandlineparser.cpp
@@ -19,6 +19,49 @@ namespace
     const QString scriptFileFlag{"script_file"};
     const QString scriptSourceFlag{"script_inline"};
     const QString localeOverrideFlag{"locale_override"};
+
+    /**
+     * @brief setupCommandLineParser Sets up a command line parser to handle application arguments.
+     * @param parser The parser to setup.
+     * @param arguments The arguments to parse.
+     */
+    void setupCommandLineParser(QCommandLineParser& parser, const QStringList& arguments)
+    {
+        parser.setApplicationDescription(geometrize::strings::Strings::getShortApplicationDescription());
+        parser.addHelpOption();
+        parser.addVersionOption();
+
+        // NOTE left these untranslated
+        parser.addOption(QCommandLineOption(scriptFileFlag, "Executes the ChaiScript script file at the given file path", "File path to ChaiScript script file"));
+        parser.addOption(QCommandLineOption(scriptSourceFlag, "Executes the inline ChaiScript source code unmodified", "Inline ChaiScript source code"));
+        parser.addOption(QCommandLineOption(localeOverrideFlag, "Overrides the locale and translation that the application launches with", "Locale code"));
+
+        if(!parser.parse(arguments)) {
+            assert(0 && "Failed to parse command line arguments");
+        }
+
+        parser.process(arguments);
+    }
+
+    /**
+     * @brief handleArgumentPairs Handles the arguments that were set on the parser.
+     * @param parser The parser to use to use.
+     */
+    void handleCommandLineArguments(QCommandLineParser& parser)
+    {
+        if(parser.isSet(scriptFileFlag)) {
+            const QString scriptPath{parser.value(scriptFileFlag)};
+            const std::string code{geometrize::util::readFileAsString(scriptPath.toStdString())};
+
+            std::unique_ptr<chaiscript::ChaiScript> engine{geometrize::script::createImageTaskEngine()};
+            geometrize::script::runScript(code, *engine);
+        } else if(parser.isSet(scriptSourceFlag)) {
+            const std::string code{parser.value(scriptSourceFlag).toStdString()};
+
+            std::unique_ptr<chaiscript::ChaiScript> engine{geometrize::script::createImageTaskEngine()};
+            geometrize::script::runScript(code, *engine);
+        }
+    }
 }
 
 namespace geometrize
@@ -26,49 +69,6 @@ namespace geometrize
 
 namespace cli
 {
-
-/**
- * @brief setupCommandLineParser Sets up a command line parser to handle application arguments.
- * @param parser The parser to setup.
- * @param arguments The arguments to parse.
- */
-void setupCommandLineParser(QCommandLineParser& parser, const QStringList& arguments)
-{
-    parser.setApplicationDescription(geometrize::strings::Strings::getShortApplicationDescription());
-    parser.addHelpOption();
-    parser.addVersionOption();
-
-    // NOTE left these untranslated
-    parser.addOption(QCommandLineOption(scriptFileFlag, "Executes the ChaiScript script file at the given file path", "File path to ChaiScript script file"));
-    parser.addOption(QCommandLineOption(scriptSourceFlag, "Executes the inline ChaiScript source code unmodified", "Inline ChaiScript source code"));
-    parser.addOption(QCommandLineOption(localeOverrideFlag, "Overrides the locale and translation that the application launches with", "Locale code"));
-
-    if(!parser.parse(arguments)) {
-        assert(0 && "Failed to parse command line arguments");
-    }
-
-    parser.process(arguments);
-}
-
-/**
- * @brief handleArgumentPairs Handles the arguments that were set on the parser.
- * @param parser The parser to use to use.
- */
-void handleCommandLineArguments(QCommandLineParser& parser)
-{
-    if(parser.isSet(scriptFileFlag)) {
-        const QString scriptPath{parser.value(scriptFileFlag)};
-        const std::string code{geometrize::util::readFileAsString(scriptPath.toStdString())};
-
-        std::unique_ptr<chaiscript::ChaiScript> engine{geometrize::script::createImageTaskEngine()};
-        geometrize::script::runScript(code, *engine);
-    } else if(parser.isSet(scriptSourceFlag)) {
-        const std::string code{parser.value(scriptSourceFlag).toStdString()};
-
-        std::unique_ptr<chaiscript::ChaiScript> engine{geometrize::script::createImageTaskEngine()};
-        geometrize::script::runScript(code, *engine);
-    }
-}
 
 bool shouldRunInConsoleMode(const QStringList& arguments)
 {
--- geometrize/script/chaiscriptcreator.cpp
+++ geometrize/script/chaiscriptcreator.cpp
@@ -12,6 +12,12 @@ void addPrintRedirect(const std::unique_ptr<chaiscript::ChaiScript>& chai)
     chai->eval(R"(global print = fun(x) { printToConsole(to_string(x)); })"); // Redirect prints to console
 }
 
+void addDefaultTypes(const std::unique_ptr<chaiscript::ChaiScript>& chai)
+{
+    chai->add(chaiscript::bootstrap::standard_library::vector_type<std::vector<int>>("IntVector"));
+    chai->add(chaiscript::bootstrap::standard_library::vector_type<std::vector<std::string>>("StringVector"));
+}
+
 }
 
 namespace geometrize
@@ -24,8 +30,7 @@ std::unique_ptr<chaiscript::ChaiScript> createDefaultEngine()
 {
     std::unique_ptr<chaiscript::ChaiScript> chai = std::make_unique<chaiscript::ChaiScript>();
 
-    chai->add(chaiscript::bootstrap::standard_library::vector_type<std::vector<int>>("IntVector"));
-    chai->add(chaiscript::bootstrap::standard_library::vector_type<std::vector<std::string>>("StringVector"));
+    addDefaultTypes(chai);
 
     chai->add(createDefaultBindings());
 
@@ -38,6 +43,8 @@ std::unique_ptr<chaiscript::ChaiScript> createLaunchWindowEngine()
 {
     std::unique_ptr<chaiscript::ChaiScript> chai = std::make_unique<chaiscript::ChaiScript>();
 
+    addDefaultTypes(chai);
+
     chai->add(createDefaultBindings());
     chai->add(createLaunchWindowBindings());
 
@@ -49,6 +56,8 @@ std::unique_ptr<chaiscript::ChaiScript> createLaunchWindowEngine()
 std::unique_ptr<chaiscript::ChaiScript> createImageTaskEngine()
 {
     std::unique_ptr<chaiscript::ChaiScript> chai = std::make_unique<chaiscript::ChaiScript>();
+
+    addDefaultTypes(chai);
 
     chai->add(createDefaultBindings());
     chai->add(createGeometrizeLibraryBindings());
@@ -65,6 +74,8 @@ std::unique_ptr<chaiscript::ChaiScript> createImageTaskEngine()
 std::unique_ptr<chaiscript::ChaiScript> createShapeMutatorEngine()
 {
     std::unique_ptr<chaiscript::ChaiScript> chai = std::make_unique<chaiscript::ChaiScript>();
+
+    addDefaultTypes(chai);
 
     chai->add(createDefaultBindings());
     chai->add(createGeometrizeLibraryBindings());
